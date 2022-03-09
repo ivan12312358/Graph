557bb1bca7ea0aeff1ee4fdc5badcc99d0d50dcb
ivan <panferovi@list.ru>
2022-03-10 02:48:44 +0300

    Added functions for dump and fill edges and vert,

--- graph.hpp
+++ graph.hpp
@@ -26,47 +26,125 @@ namespace graph
 		{
 			size_t v_count = 0;
 
-			for (auto it = vertex.begin(), itt = vertex.end(); it != itt; ++it)
+			for (auto&& it: vertex)
 			{
-				if (vertices.insert({it->first, v_count}).second)
+				if (vertices.insert({it.first, v_count}).second)
 					v_count++;
 
-				if (vertices.insert({it->second, v_count}).second)
+				if (vertices.insert({it.second, v_count}).second)
 					v_count++;
 			}
 
 			Table.resize(v_count);
 
-			for (auto it = vertex.begin(), itt = vertex.end(); it != itt; ++it)
+			for (auto&& it: vertex)
 			{
-				AddEdge(it->first,  vertices.find(it->first)->second);
-				AddEdge(it->second, vertices.find(it->second)->second);
+				AddEdge(it.first,  GetVIndex(it.first));
+				AddEdge(it.second, GetVIndex(it.second));
 			}
-	
-/* 			{
-				for (size_t i = 0; i < Table.size(); ++i)
-					std::cout << i << '\t';
-
-				std::cout << '\n';
-
-				for (size_t j = 0; j < 3; ++j)
-				{
-					for (size_t i = 0; i < Table.size(); ++i)
-						std::cout << Table[i][j] << '\t';
-
-					std::cout << '\n';
-				}
-			}
- */		
 		}
 
-		void FillData(int vert, VL vdata) noexcept {}
-		void FillData(std::pair<int, int> edge, EL edata) noexcept {}
+		void FillData(int vert, VL vdata) noexcept 
+		{
+			if (Vdata.empty())
+				Vdata.resize(vertices.size());
+
+			int it = GetVIndex(vert);
+			if (it != -1) Vdata[it] = vdata;
+		}
+
+		void FillData(std::pair<int, int> edge, EL edata) noexcept 
+		{
+			//reserve(Table,size() - vert.size())
+			if (Edata.empty())
+				Edata.resize(Table.size() - vertices.size());
+
+			int it = GetEIndex(edge);
+			if (it != -1) Edata[it] = edata;
+		}
 		// breadth search
 		void BFS();
 		// depth search
 		void DFS();
-		// 
+
+		int GetEIndex(const std::pair<int, int>& edge) const 
+		{
+			for (size_t it = vertices.size(); it < Table.size(); ++it)
+			{
+				if (Table[it][0] 	 == edge.first && 
+					Table[it ^ 1][0] == edge.second)
+					return it - vertices.size();
+			}
+
+			return -1;
+		}
+
+		int GetVIndex(int vert) const
+		{ 
+			auto it = vertices.find(vert);
+
+			if (it != vertices.cend())
+				return it->second;
+
+			return -1;
+		}
+
+		void Dump()
+		{
+			PrintTable();
+			PrintVertices();
+			PrintEdges();
+		}
+
+		void PrintTable()
+		{
+			for (size_t i = 0; i < Table.size(); ++i)
+				std::cout << i << '\t';
+
+			std::cout << '\n';
+
+			for (size_t j = 0; j < 3; ++j)
+			{
+				for (size_t i = 0; i < Table.size(); ++i)
+					std::cout << Table[i][j] << '\t';
+
+				std::cout << '\n';
+			}
+		}
+
+		void PrintVertices()
+		{
+			if (!Vdata.empty())
+			{
+				for (auto&& it: vertices)
+					std::cout << "(" << it.first << ", " << Vdata[it.second] << ") ";
+			}
+			else
+			{
+				for (auto&& it: vertices)
+					std::cout << it.first << ' ';
+			}
+
+			std::cout << '\n';
+		}
+
+		void PrintEdges()
+		{
+			if (!Edata.empty())
+			{
+				for (size_t it = vertices.size(); it < Table.size(); ++it)
+					std::cout << "({"  << Table[it][0] << ", " << Table[it ^ 1][0] 
+							  << "}, " << Edata[it - vertices.size()] << ") ";
+			}
+			else 
+			{
+				for (size_t it = vertices.size(); it < Table.size(); ++it)
+					std::cout << "{"  << Table[it][0] << ", " << Table[it ^ 1][0] << "} ";
+			}
+
+			std::cout << '\n';
+		}
+
 	private:
 		void AddEdge (int vert, int index) noexcept
 		{
--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 
 int main()
 {
-    graph::Graph kgraph {{1,2},{1,3},{2,3},{2,4},{3,4}};
+    graph::Graph<int, int> kgraph {{1,2},{1,3},{2,3},{2,4},{3,4}};
+
+    kgraph.FillData(1, 123);
+    kgraph.FillData(2, 71);
+    kgraph.FillData(3, 216);
+    kgraph.FillData(4, 584);
+
+    kgraph.FillData({1, 4}, 584);
+    kgraph.FillData({1, 2}, 212);
+    kgraph.FillData({1, 3}, 754);
+    kgraph.FillData({4, 2}, 1000);
+    kgraph.FillData({5, 7}, 12000);
+
+
+    kgraph.Dump();
 
     return 0;
 }
