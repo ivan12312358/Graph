de29118b577652d5574d533300517b37769942ef
ivan <panferovi@list.ru>
2022-03-25 21:21:03 +0300

    Added function that cheks is graph is bipartitate.

--- Graph/graph.hpp
+++ Graph/graph.hpp
@@ -1,8 +1,11 @@
-#include <unordered_map>
+#include <map>
 #include <vector>
 #include <array>
 #include <iostream>
 #include <iomanip>
+
+//TODO	GetVcolor()
+//		IsSameColors()
 
 namespace graph
 {
@@ -10,18 +13,45 @@ namespace graph
 	constexpr bool is_edge_data = false;
 	
 	template <typename T>
-	constexpr bool is_edge_data<T, std::void_t< decltype(std::get<2>(std::declval<T&>()))>> = true;
+	constexpr bool is_edge_data<T, std::void_t<decltype(std::get<2>(std::declval<T&>()))>> = true;
 
-	enum class color
+	enum class Color
 	{
-		red,
-		blue
+		colorless,
+		blue,
+		red
 	};
 
-	template <typename EL = int, typename VL = color>
+	std::ostream& operator << (std::ostream& out, Color color)
+	{
+		switch(color)
+		{
+			case Color::red:
+				std::cout << 'r';
+				break;
+			case Color::blue:
+				std::cout << 'b';
+				break;
+			case Color::colorless:
+				std::cout << "colorless";
+				break;
+			default:
+				break;
+		}
+
+		return out;
+	}
+
+	void SwitchColor(Color& color)
+	{
+		color = color == Color::blue ? Color::red : 
+									   Color::blue;
+	}
+
+	template <typename EL = int, typename VL = int>
 	class Graph final
 	{
-		std::unordered_map<int, int> vertices;
+		std::map<int, int> vertices;
 		std::vector<std::array<int, 3>> Table;
 		std::vector<EL> Edata;
 		std::vector<VL> Vdata;
@@ -53,8 +83,8 @@ namespace graph
 
 			for (auto it = begin; it != end; ++it)
 			{
-				AddEdge(std::get<0>(*it), GetVIndex(std::get<0>(*it)));
-				AddEdge(std::get<1>(*it), GetVIndex(std::get<1>(*it)));
+				AddEdge(std::get<0>(*it));
+				AddEdge(std::get<1>(*it));
 
 				if constexpr (is_edge_data<typename std::iterator_traits<Iter>::value_type>)
 					Edata.push_back(std::get<2>(*it));
@@ -64,7 +94,7 @@ namespace graph
 		Graph (std::initializer_list<std::pair<int, int>> edges) 	  noexcept: Graph(edges.begin(), edges.end()) {}
 		Graph (std::initializer_list<std::tuple<int, int, EL>> edges) noexcept: Graph(edges.begin(), edges.end()) {}
 
-		void FillData(int vert, const VL& vdata) noexcept 
+		void FillData(int vert, const VL& vdata) noexcept
 		{
 			if (Vdata.empty())
 				Vdata.resize(vertices.size());
@@ -87,12 +117,14 @@ namespace graph
 				Edata[it] = edata;
 		}
 
-		int GetEIndex(const std::pair<int, int>& edge) const 
+		int GetEIndex(const std::pair<int, int>& edge) const
 		{
+			static int offset = (vertices.size() % 2) == 0 ? 0 : 2;
+
 			for (size_t it = vertices.size(); it < Table.size(); it += 2)
 			{
-				if (Table[it][0] 	 == edge.first && 
-					Table[it ^ 1][0] == edge.second)
+				if (Table[it][0] 	 			== edge.first && 
+					Table[(it ^ 1) + offset][0] == edge.second)
 					return it - vertices.size();
 			}
 
@@ -119,16 +151,16 @@ namespace graph
 		void PrintTable() const
 		{
 			for (size_t i = 0; i < Table.size(); ++i)
-				std::cout << i << std::setw(6);
+				std::cout << i << " \t";
 
-			std::cout << '\n';
+			std::cout << std::endl;
 
 			for (size_t j = 0; j < 3; ++j)
 			{
 				for (size_t i = 0; i < Table.size(); ++i)
-					std::cout << Table[i][j] << std::setw(6);
+					std::cout << Table[i][j] << " \t";
 
-				std::cout << '\n';
+				std::cout << std::endl;
 			}
 		}
 
@@ -145,34 +177,134 @@ namespace graph
 					std::cout << it.first << ' ';
 			}
 
-			std::cout << '\n';
+			std::cout << std::endl;
 		}
 
 		void PrintEdges() const
 		{
+			int offset = (vertices.size() % 2) == 0 ? 0 : 2;
+
 			if (!Edata.empty())
 			{
 				for (size_t it = vertices.size(), eit = 0; it < Table.size(); it += 2, eit++)
-					std::cout << "({"  << Table[it][0] << ", " << Table[it ^ 1][0] 
+					std::cout << "({"  << Table[it][0] << ", " << Table[(it ^ 1) + offset][0] 
 							  << "}, " << Edata[eit] << ") ";
 			}
 			else 
 			{
-				for (size_t it = vertices.size(); it < Table.size(); ++it)
-					std::cout << "{"  << Table[it][0] << ", " << Table[it ^ 1][0] << "} ";
+				for (size_t it = vertices.size(); it < Table.size(); it += 2)
+					std::cout << "{"  << Table[it][0] << ", " << Table[(it ^ 1) + offset][0] << "} ";
 			}
 
-			std::cout << '\n';
+			std::cout << std::endl;
 		}
 
-		// breadth search
 		void BFS();
-		// depth search
-		void DFS();
+
+		bool IsBipartite()
+		{
+			if (Table.empty())
+				return 0;
+
+			std::vector<Color> colors(vertices.size());
+			int edge{};
+
+			for (size_t it = 0, itt = vertices.size(); it != itt; ++it)
+			{
+				edge = Table[it][1];
+
+				if (!Painted(colors, Table[edge][0]))
+					if (PaintGraph(colors, edge) == 0)
+						return 0;
+			}
+
+			auto cit = colors.begin();
+
+			for (auto&& vit = vertices.begin(), vitt = vertices.end(); vit != vitt; ++vit, ++cit)
+				std::cout << vit->first << ' ' << *cit << ' ';
+
+			std::cout << std::endl;
+
+			return 1;
+		}
 
 	private:
-		void AddEdge (int vert, int index) noexcept
+		bool Painted(std::vector<Color>& colors, int vert)
 		{
+			if (int it = GetVIndex(vert); it != -1)
+				return colors[it] != Color::colorless;
+	
+			return 0;
+		}
+
+		bool PaintGraph(std::vector<Color>& colors, int edge)
+		{
+			static int offset = (vertices.size() % 2) == 0 ? 0 : 2;
+
+			Color vcolor = Color::blue, nxtcolor;
+			int nxtedge{}, fstedge = Table[edge][2];
+
+			std::vector<int> prevedge;
+			prevedge.reserve(Table.size() - vertices.size());
+
+			while (true)
+			{
+				prevedge.push_back(edge);
+
+				if (!Painted(colors, Table[edge][0]))
+				{
+					Paint(colors, Table[edge][0], vcolor);
+					SwitchColor(vcolor);
+				}
+				
+				if ((edge ^ 1) + offset < Table.size())
+				{
+					nxtedge = (edge ^ 1) + offset;
+
+					if (Painted(colors, Table[nxtedge][0]))
+					{
+						if (Table[edge][2] == fstedge || Table[edge][1] == fstedge)
+							return GetVColor(Table[edge][0]) != vcolor;
+					}
+					else if (Table[nxtedge][1] < vertices.size())
+						Paint(colors, Table[nxtedge][0], vcolor);
+					else 
+					{
+						edge = Table[nxtedge][1];
+						continue;
+					}
+				}
+
+				edge   = GetPrevEdge(prevedge);
+				vcolor = colors[GetVIndex(Table[edge][0])];
+				SwitchColor(vcolor);
+			}
+		}
+
+		int GetPrevEdge(std::vector<int>& prevedge)
+		{
+			int edge{};
+
+			while (!prevedge.empty())
+			{
+				edge = prevedge.back();
+
+				if (Table[edge][1] < vertices.size())
+					prevedge.pop_back();
+				else return Table[edge][1];
+			}
+
+			return edge;
+		}
+
+		void Paint(std::vector<Color>& colors, int vert, Color color) { colors[GetVIndex(vert)] = color; }
+
+		Color GetVColor(std::vector<Color>& colors, int vert) { return colors[GetVIndex(vert)]; }
+
+		void AddEdge (int vert) noexcept
+		{
+			int index = GetVIndex(vert);
+
 			if (Table[index][2] == 0)
 			{
 				Table.push_back({vert, index, index});
--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include "driver.hpp"
 #include <fstream>
 
+#define DEBUG 1
+
 int main(int argc, char** argv)
 {
+#ifndef DEBUG
     if (argc != 2)
     {
         std::cout << "Correct usage example: ./Graph <filename>" << std::endl;
@@ -15,7 +18,7 @@ int main(int argc, char** argv)
     if (!fEdges.is_open())
     {
         std::cout << "Couldn't open file." << std::endl;
-        return 0;
+        return 1;
     }
 
     std::streambuf* cin_buff = std::cin.rdbuf();
@@ -24,15 +27,28 @@ int main(int argc, char** argv)
     yy::Driver driver;
 
     try { driver.parse(); }
-    catch(const std::exception& e)
+    catch (const std::exception& e)
     {
         std::cerr << e.what() << '\n';
     }
 
     std::cin.rdbuf (cin_buff);
+    graph::Graph kgraph(driver.begin(), driver.end());
+#endif
 
-    graph::Graph<int, int> kgraph (driver.begin(), driver.end());
+    // graph::Graph kgraph {{1, 2}, {1, 3}, {2, 3}};
+    // graph::Graph kgraph {{3, 2}, {3, 1}, {2, 1}};
+    // graph::Graph kgraph {{1, 2}, {2, 3}, {3, 4}, {4, 1}, {6, 7}, {6, 8}, {8, 9}};
+    // graph::Graph kgraph {{1, 2}, {2, 3}};
+    // graph::Graph kgraph {{4, 1}, {4, 3}, {3, 2}, {2, 1}};
+    // graph::Graph kgraph {{1, 2}, {1, 3}, {3, 4}};
+    graph::Graph kgraph {{1, 1}};
+
     kgraph.Dump();
+
+    if (kgraph.IsBipartite() == 0)
+        std::cout << "Error: not a bipartite graph" << std::endl;
+
 
     return 0;
 }
